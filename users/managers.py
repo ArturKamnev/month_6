67415ffdc84ac0965f7b6109b93e1d90655ff06c
ArturKamnev <kamnevartur28@gmail.com>
2026-07-24 17:04:33 +0600

    Первый урок

---
+++ users/managers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_stuff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have stuff permission')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have superuser permission')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have stuff permission')
+        
+        return self.create_user(email, password, **extra_fields)
