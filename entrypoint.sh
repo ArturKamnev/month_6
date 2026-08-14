af10ae3a794958e863c14bd8ace247e648a70620
ArturKamnev <kamnevartur28@gmail.com>
2026-08-14 18:29:49 +0600

    Восьмой урок

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+exec gunicorn shop_api.wsgi:application --bind 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
